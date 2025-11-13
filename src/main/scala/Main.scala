06db2ac35bec46033f7d7f5bb72860676f8906fb
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-11-13 07:42:14 +0000

    add kafka consumer

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
-import cats.NonEmptyParallel
 import cats.data.Kleisli
 import cats.effect.*
 import cats.effect.IO
 import cats.effect.IOApp
 import cats.implicits.*
 import cats.syntax.all.*
+import cats.NonEmptyParallel
 import com.comcast.ip4s.*
 import configuration.AppConfig
 import configuration.ConfigReader
@@ -12,37 +12,39 @@ import controllers.NotificationSocketRoutes
 import dev.profunktor.redis4cats.RedisCommands
 import doobie.hikari.HikariTransactor
 import doobie.util.ExecutionContexts
-import fs2.Stream
 import fs2.concurrent.Topic
+import fs2.kafka.ConsumerSettings
+import fs2.Stream
 import infrastructure.KafkaProducerProvider
+import java.time.Instant
+import java.util.UUID
 import middleware.Middleware.throttleMiddleware
 import models.*
 import modules.*
-import org.http4s.HttpRoutes
-import org.http4s.Method
-import org.http4s.Request
-import org.http4s.Response
-import org.http4s.Uri
 import org.http4s.client.Client
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.headers.Origin
 import org.http4s.implicits.*
+import org.http4s.server.middleware.CORS
 import org.http4s.server.Router
 import org.http4s.server.Server
-import org.http4s.server.middleware.CORS
+import org.http4s.HttpRoutes
+import org.http4s.Method
+import org.http4s.Request
+import org.http4s.Response
+import org.http4s.Uri
 import org.typelevel.ci.CIString
-import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.Logger
 import repositories.*
 import routes.Routes.*
-import services.*
-import services.OutboxPublisherService
-
-import java.time.Instant
-import java.util.UUID
 import scala.concurrent.duration.*
 import scala.concurrent.duration.DurationInt
+import services.*
+import services.OutboxPublisherService
+import fs2.kafka.AutoOffsetReset
+import consumers.kafka.consumers.NotificationConsumer
 
 object Main extends IOApp {
 
@@ -61,6 +63,14 @@ object Main extends IOApp {
         host: Host <- Resource.eval(IO.fromOption(Host.fromString(config.serverConfig.host))(new RuntimeException("Invalid host in configuration")))
         port: Port <- Resource.eval(IO.fromOption(Port.fromInt(config.serverConfig.port))(new RuntimeException("Invalid port in configuration")))
 
+        notificationRepo = NotificationRepository[IO](transactor)
+        // 3️⃣ Set up Kafka consumer configuration
+        kafkaConsumerSettings =
+          ConsumerSettings[IO, String, String]
+            .withBootstrapServers(config.kafkaConfig.bootstrapServers)
+            .withGroupId("notification-service")
+            .withAutoOffsetReset(AutoOffsetReset.Earliest)
+
         initNotification =
           Notification(
             id = UUID.randomUUID().toString,
@@ -75,6 +85,9 @@ object Main extends IOApp {
         // ✅ Create Topic inside Resource
         topic <- Resource.eval(Topic[IO, Notification])
         socketRoutes = new NotificationSocketRoutes[IO](topic)
+        consumer = new NotificationConsumer[IO](topic, notificationRepo, kafkaConsumerSettings)
+        _ <- Resource.eval(consumer.stream.compile.drain.start)
+
 
         // ✅ Combine REST + WebSocket routes
         server <- EmberServerBuilder
